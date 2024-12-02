ec74aa0f229ff57856c70ed1a54e1de5506b965f
NimmalaNithin <60269489+NimmalaNithin@users.noreply.github.com>
2024-12-02 17:28:04 -0500

    learnt to read and display text from form

---
+++ src/main/java/com/springmvc/thymeleaf/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.springmvc.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping ("/show-form")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    @RequestMapping ("/process-form")
+    public String processForm(){
+        return "helloworld";
+    }
+
+}

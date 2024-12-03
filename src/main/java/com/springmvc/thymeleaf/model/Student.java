ea2a163009fc50d3fdb9159339c8fad6e8c98582
NimmalaNithin <60269489+NimmalaNithin@users.noreply.github.com>
2024-12-02 21:01:13 -0500

    learnt to bind data from's dropdown

--- src/main/java/com/springmvc/thymeleaf/model/Student.java
+++ src/main/java/com/springmvc/thymeleaf/model/Student.java
@@ -3,12 +3,14 @@ package com.springmvc.thymeleaf.model;
 public class Student {
     private String firstName;
     private String lastName;
+    private String Country;
 
     public Student(){}
 
-    public Student(String firstName, String lastName) {
+    public Student(String firstName, String lastName, String country) {
         this.firstName = firstName;
         this.lastName = lastName;
+        Country = country;
     }
 
     public String getFirstName() {
@@ -25,5 +27,13 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
     }
 }

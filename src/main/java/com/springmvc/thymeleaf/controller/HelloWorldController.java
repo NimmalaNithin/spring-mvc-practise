fb3f3c273e91e11b3235d95a8870603bd74df63b
NimmalaNithin <60269489+NimmalaNithin@users.noreply.github.com>
2024-12-02 19:08:55 -0500

    learnt to pass data using @GetMapping and @PostMapping

--- src/main/java/com/springmvc/thymeleaf/controller/HelloWorldController.java
+++ src/main/java/com/springmvc/thymeleaf/controller/HelloWorldController.java
@@ -3,13 +3,15 @@ package com.springmvc.thymeleaf.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping ("/show-form")
+    @GetMapping("/show-form")
     public String showForm(){
         return "helloworld-form";
     }
@@ -28,10 +30,10 @@ public class HelloWorldController {
         return "helloworld";
     }
 
-    @RequestMapping ("/process-form-v3")
+    @PostMapping("/process-form-v3")
     public String processFormV3(@RequestParam("studentName") String theName, Model model){
         theName = theName.toUpperCase();
-        String result = "Yo! " + theName;
+        String result = "from v3! " + theName;
         model.addAttribute("message" , result);
         return "helloworld";
     }
